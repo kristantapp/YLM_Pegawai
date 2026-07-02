0731576ddeac4e5e17b337bb3aa41d5f0603c052
kristantapp <kristant.app@gmail.com>
2026-07-02 09:41:17 +0700

    Update CACHE_NAME to ylm-pegawai-v1

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'ylm-pegawai-v1';
+const urlsToCache = [
+  '/',                // Membaca halaman utama otomatis di Vercel
+  '/index.html',
+  '/manifest.json',
+  '/logo_192_YLM.png',
+  '/logo_512_YLM.png',
+  '/logo_180_YLM.png'
+];
+
+// Install Service Worker dan simpan aset ke cache browser
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Membuka cache dan menyimpan aset...');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ambil data dari cache jika sedang offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Gunakan aset dari cache jika ada
+        }
+        return fetch(event.request); // Jika tidak ada, ambil dari internet
+      })
+  );
+});
